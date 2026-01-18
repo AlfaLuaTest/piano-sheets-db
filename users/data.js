e7e8b6db27c195f5d439c3257eca4e60f7fe8589
ORBI <118558391+AlfaLuaTest@users.noreply.github.com>
2026-01-18 15:50:00 +0300

    Update favorites for user 918031124

--- users/data.js
+++ users/data.js
@@ -1,12 +1,7 @@
 // Auto-generated favorites list
 // Multi-user support - each user has their own favorites
-// Updated: 2026-01-17 00:00:00 UTC
+// Updated: 2026-01-18 12:49:59 UTC
 
 export const favorites = {
-  // User favorites will be added here automatically
-  // Format: "user_id": ["song-id-1", "song-id-2", ...]
-  
-  // Example users:
-  // "user_123456789": ["kid-cudi", "bohemian-rhapsody"],
-  // "user_987654321": ["starboy", "blinding-lights"]
+  "918031124": ["pork-soda"]
 };
