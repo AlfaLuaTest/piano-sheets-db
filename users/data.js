426618023a7686426a499f1f4ed9d3e576f62973
ORBI <118558391+AlfaLuaTest@users.noreply.github.com>
2026-01-19 00:14:48 +0300

    Update favorites for user 918031124

--- users/data.js
+++ users/data.js
@@ -1,7 +1,7 @@
 // Auto-generated favorites list
 // Multi-user support - each user has their own favorites
-// Updated: 2026-01-18 21:11:50 UTC
+// Updated: 2026-01-18 21:14:48 UTC
 
 export const favorites = {
-  "918031124": ["grandmas-home", "stick-season", "pork-soda"]
+  "918031124": ["grandmas-home", "stick-season"]
 };
