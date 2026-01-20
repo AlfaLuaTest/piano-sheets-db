e14590a11a2f1b116ffc043578e9fd71a981aefd
ORBI <118558391+AlfaLuaTest@users.noreply.github.com>
2026-01-20 23:44:48 +0300

    Update favorites for user 347604851

--- users/data.js
+++ users/data.js
@@ -1,6 +1,6 @@
 // Auto-generated favorites list
 // Multi-user support - each user has their own favorites
-// Updated: 2026-01-20 20:44:45 UTC
+// Updated: 2026-01-20 20:44:48 UTC
 
 export const favorites = {
   "918031124": ["pork-soda", "another-love", "promise", "like-him", "stick-season"],
@@ -8,5 +8,5 @@ export const favorites = {
   "919705100": ["kid-cudi", "married-life", "be-nice-2-me"],
   "915931217": [],
   "691546047": ["pork-soda", "the-place-where-it-rained"],
-  "347604851": ["hype-boy", "call-of-silence", "guren-no-yumiya", "red-swan", "akuma-no-ko"]
+  "347604851": ["hype-boy", "call-of-silence", "guren-no-yumiya", "red-swan", "akuma-no-ko", "shinzo-wo-sasageyo"]
 };
