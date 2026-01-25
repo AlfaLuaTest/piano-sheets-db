319e260a51a50298b1dc22544a7a3562fb37aeb7
ORBI <118558391+AlfaLuaTest@users.noreply.github.com>
2026-01-25 12:09:17 +0300

    Update favorites for user 845436524

--- users/data.js
+++ users/data.js
@@ -1,6 +1,6 @@
 // Auto-generated favorites list
 // Multi-user support - each user has their own favorites
-// Updated: 2026-01-25 08:58:16 UTC
+// Updated: 2026-01-25 09:09:16 UTC
 
 export const favorites = {
   "918031124": ["pork-soda", "another-love", "promise", "like-him", "stick-season"],
@@ -9,7 +9,7 @@ export const favorites = {
   "915931217": ["golden-hour"],
   "691546047": ["pork-soda", "the-place-where-it-rained"],
   "347604851": ["hype-boy", "call-of-silence", "guren-no-yumiya", "red-swan", "akuma-no-ko", "shinzo-wo-sasageyo"],
-  "845436524": ["stick-season", "song-of-storms", "resonanse-heavens-secret-requiem", "five-nights-at-freddys-movie", "passionfruit", "one", "get-jinxed"],
+  "845436524": ["stick-season", "song-of-storms", "resonanse-heavens-secret-requiem", "five-nights-at-freddys-movie", "passionfruit", "one", "get-jinxed", "impostor-syndrome"],
   "708988738": ["unravel", "bling-bang-bang-born", "gurenge"],
   "228438939": ["lonely-andreas-ronnberg"],
   "99737190": ["aria-math"],
